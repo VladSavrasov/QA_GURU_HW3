a39457045a74de594d91ebf6fe607be54b03e9ad
Vlad Savrasov <vsvsvs08@rambler.ru>
2023-09-20 20:27:57 +0300

    custom changes

--- src/test/java/HW5Test.java
+++ src/test/java/HW5Test.java
@@ -1,14 +1,16 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.DragAndDropOptions.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class HW5Test {
     @Test
-            public void shouldHaveTextBuildLikeTheBest(){
+    public void shouldHaveTextBuildLikeTheBest() {
         open("https://github.com");
         $(".header-menu-wrapper").$(byText("Solutions")).hover();
         $(".header-menu-wrapper").$(byTagAndText("a", "Enterprise")).click();
@@ -16,9 +18,11 @@ public class HW5Test {
     }
 
     @Test
-    public void dragAndDrop(){
+    public void dragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").$("header").shouldHave(text("A"));
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(to("#column-b"));
         $("#column-a").$("header").shouldHave(text("B"));
         $("#column-b").$("header").shouldHave(text("A"));
     }
